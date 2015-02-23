67ebfdd7301c46321065f905fdb06f01bea466dd
Philip Wu <phillupw@gmail.com>
2015-02-22 16:03:40 -0800

    To Turn In

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of makeCacheMatrix and cacheSolve is to hold a matrix 
+##and cache the inverse for quick retrieval
 
-## Write a short comment describing this function
+## makeCacheMatrix stores a matrix and functions needed to retrieve it and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL #Inverse
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) i <<- inverse
+  getInverse <- function() i
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## solves for matrix inverse. if inverse is already cached, retrieves it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getInverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setInverse(i)
+  i
 }
